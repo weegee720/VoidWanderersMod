6524fe09d83cfc874b03b6522646b5b3912e4f67
weegee <40016344+weegee720@users.noreply.github.com>
2020-05-24 18:41:26 +0300

    B104

---
+++ VoidWanderers.rte/Extensions/Patch_Untitled.lua
@@ -0,0 +1,25 @@
+if PresetMan:GetModuleID("Untitled.rte") ~= -1 then
+	function CF_MakeItem2(item, class)
+		--print ("CF_MakeItem2 - PATCHED")
+		-- print ("CF_MakeItem")
+		if class == nil then
+			class = "HDFirearm"
+		end
+		
+		if class == "HeldDevice" then
+			return CreateHeldDevice(item)
+		elseif class == "HDFirearm" then
+			if item == "Laser Rifle" then
+				return CreateHDFirearm(item, "Untitled.rte")
+			else
+				return CreateHDFirearm(item)
+			end
+		elseif class == "TDExplosive" then
+			return CreateTDExplosive(item)
+		elseif class == "ThrownDevice" then
+			return CreateThrownDevice(item)
+		end
+		
+		return nil;
+	end
+end
